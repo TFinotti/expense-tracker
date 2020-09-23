350ef4e1fd7e177b56df3447b632ebaf59e45cc9
Tiago <tfrancof@my.centennialcollege.ca>
2020-09-23 12:43:45 -0400

    HTML and CSS files into React Components

---
+++ src/components/Breakdown.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Breakdown = () => {
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Tiago</h4>
+        <p>
+          $0.00
+        </p>
+      </div>
+      <div>
+        <h4>Maryam</h4>
+        <p>
+          $0.00
+        </p>
+      </div>
+      <div>
+        <h4>Total</h4>
+        <p>
+          $0.00
+        </p>
+      </div>
+    </div>
+  );
+};
