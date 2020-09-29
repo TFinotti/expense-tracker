11a0c372c8d35b85f3490abe9957e04fa22c4d9c
Tiago <tfrancof@my.centennialcollege.ca>
2020-09-29 13:11:34 -0400

    Introduced Global State to the Breakdown Component Implemented Delete button

--- src/components/Breakdown.js
+++ src/components/Breakdown.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Breakdown = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Tiago</h4>
         <p>
-          $0.00
+          ${income}
         </p>
       </div>
       <div>
         <h4>Maryam</h4>
         <p>
-          $0.00
+          ${expense}
         </p>
       </div>
       <div>
         <h4>Total</h4>
         <p>
-          $0.00
+          ${total}
         </p>
       </div>
     </div>
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer } from "react";
-import AppReducer from './AppReducer';
+import AppReducer from "./AppReducer";
 
 // Initial state
 
@@ -21,10 +21,20 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  // Actions
+
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
       }}
     >
       {children}
